2ca325142387f438ca7a56a0122b8c21aa8ffd2f
sonuhalder24 <haldersonu36@gmail.com>
2021-06-03 12:13:08 +0530

    some designing added

--- app/src/main/java/com/example/atry/SelectActivity.java
+++ app/src/main/java/com/example/atry/SelectActivity.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class SelectActivity extends AppCompatActivity {
-    Button type,pdf;
+    ImageView type,pdf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
